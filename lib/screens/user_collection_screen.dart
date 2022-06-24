408110901bafaf8dac40629b9dcdbab740dce2d1
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2022-06-24 18:59:56 -0400

    Can now display all data for users flashcards

--- lib/screens/user_collection_screen.dart
+++ lib/screens/user_collection_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -19,14 +20,29 @@ class _UserCollectionScreenState extends State<UserCollectionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
+    return Scaffold(
+      appBar: AppBar(title: const Text("Collection")),
+      body: StreamBuilder<QuerySnapshot>(
         stream: _flashcardsStream,
         builder: (BuildContext context,
             AsyncSnapshot<QuerySnapshot> flashcardSnapshot) {
+          if (flashcardSnapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading');
+          }
           List flashcardsList = flashcardSnapshot.data!.docs.toList();
-          print(flashcardsList);
-          print(flashcardsList[1].data());
-          return Text("Flashcard");
-        });
+          return ListView.builder(
+            itemCount: flashcardsList.length,
+            itemBuilder: (context, index) {
+              LinkedHashMap<String, dynamic> flashcardData =
+                  flashcardsList[index].data();
+              return ListTile(
+                key: UniqueKey(),
+                title: Text(flashcardData["target_language"]),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
